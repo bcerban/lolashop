a8457c3ae2ef619b071c7d6b24166816a69623b5
Bettina <bettinacerban@gmail.com>
2019-06-16 16:39:03 -0300

    install apollo libraries & dependencies

--- src/Shop.js
+++ src/Shop.js
@@ -1,10 +1,21 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import MainNavigation from './components/navigator';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
-export default class Shop extends Component {
-    render() {
-        return (
-            <MainNavigation />
-        );
-    }
-}
+const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: new HttpLink({
+      uri: 'http://localhost:4000/',
+    }),
+  });
+
+const Shop = () => (
+    <ApolloProvider client={client}>
+        <MainNavigation />
+    </ApolloProvider>
+);
+
+export default Shop;
