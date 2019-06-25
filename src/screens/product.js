b717ed096689a324fad43d59874c9cf496fa967f
Bettina <bettinacerban@gmail.com>
2019-06-24 23:02:44 -0300

    add product description

--- src/screens/product.js
+++ src/screens/product.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import { Text, View, Dimensions, StyleSheet, Platform } from 'react-native';
-import { withTheme, Title } from 'react-native-paper';
+import { Text, View, Dimensions, StyleSheet, Platform, Alert } from 'react-native';
+import { withTheme, Title, Paragraph, Button } from 'react-native-paper';
 import { Layout } from '../containers';
 import Carousel, { Pagination, ParallaxImage } from 'react-native-snap-carousel';
 
@@ -19,16 +19,16 @@ class Product extends Component {
     }
 
     renderImage = ({ item, index }, parallaxProps) => (
-        <View style={{ width: screenWidth - padding * 2, height: screenWidth - padding * 2 }}>
+        <View style={{ width: screenWidth, height: screenWidth }}>
             <ParallaxImage
                 source={{ uri: item.uri }}
-                dimensions={{ width: screenWidth - padding * 2, height: screenWidth - padding * 2 }}
+                dimensions={{ width: screenWidth, height: screenWidth }}
                 parallaxFactor={0.4}
                 showSpinner={true}
                 containerStyle={{
                     flex: 1,
                     marginBottom: Platform.select({ ios: 0, android: 1 }), 
-                    backgroundColor: this.props.theme.colors.background,
+                    backgroundColor: 'white',
                     borderRadius: this.props.theme.roundness,
                 }}
                 style={{
@@ -46,35 +46,57 @@ class Product extends Component {
             : [];
 
         return (
-            <Layout title={this.state.name}>
-                {/* <View>
-                    <Title>{this.state.name}</Title>
-                </View> */}
-
-                <View style={{ paddingBottom: padding, height: screenWidth + padding * 2 + 15 }}>
+            <Layout justifyContent='flex-start' scrollable={true}>
+                <View style={{ 
+                    height: screenWidth, 
+                    border: `1px solid ${this.props.theme.colors.background}`,
+                    backgroundColor: 'transparent' 
+                }}>
                     <Carousel
                         ref={c => this.carousel = c}
                         sliderWidth={screenWidth}
                         sliderHeight={screenWidth}
-                        itemWidth={screenWidth - padding * 2}
+                        itemWidth={screenWidth}
                         data={images}
                         renderItem={this.renderImage}
                         onSnapToItem={(index) => this.setState({ activeDotIndex: index })}
                         hasParallaxImages={true}
                         loop={true}
                     />
-                    <Pagination
+                    {/* <Pagination
                         dotsLength={images.length}
                         activeDotIndex={this.state.activeDotIndex}
-                        containerStyle={{ backgroundColor: this.props.theme.colors.background }}
+                        // containerStyle={{ backgroundColor: 'transparent' }}
                         dotColor={this.props.theme.colors.primary}
                         dotStyle={{ height: 10, width: 10, borderRadius: 5 }}
                         inactiveDotColor={this.props.theme.colors.text}
+                        inactiveDotStyle={{ height: 10, width: 10, borderRadius: 5 }}
                         carouselRef={this.carousel}
                         style={{ height: 10 }}
-                    />
+                    /> */}
                 </View>
-                
+            
+                <View style={{ width: screenWidth }}>
+                    <Title style={{ 
+                        textAlign: 'center',
+                        color: this.props.theme.colors.primary,
+                        backgroundColor: 'white',
+                        padding: 10,
+                        fontWeight: 'bold'
+                    }}>
+                        {this.state.name}
+                    </Title>
+                    <Paragraph style={{ padding: 10 }}>{this.state.description}</Paragraph>
+                    <Paragraph style={{ padding: 10, fontWeight: 'bold' }}>Dimensiones: {this.state.dimensions}</Paragraph>
+                    <Button 
+                        icon="shopping-cart" 
+                        mode="contained" 
+                        onPress={() => Alert.alert('', 'Agregaste un producto a tu carrito!')}
+                        style={{ width: '80%', marginLeft: '10%' }}
+                    >
+                        Agregar al carrito
+                    </Button>
+                </View>
             </Layout>
         );
     }
